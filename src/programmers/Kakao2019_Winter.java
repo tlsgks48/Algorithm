4a09040770b462850f71f6a1eac24af023a54443
tlsgks48 <tlsgks486@gmail.com>
2020-07-30 22:06:28 +0900

    프로그래머스 레벨1 3번째

--- src/programmers/Kakao2019_Winter.java
+++ src/programmers/Kakao2019_Winter.java
@@ -14,7 +14,6 @@ public class Kakao2019_Winter {
 		int[] moves = 	{1,5,3,5,1,2,1,4};
 		
 		List<Integer> basket = new ArrayList<Integer>();
-		
 		for(int i=0; i<5; i++) {
 			for(int j=0; j<5; j++) {
 				System.out.print("["+board[i][j]+"]");
@@ -44,6 +43,7 @@ public class Kakao2019_Winter {
 		
 		System.out.println(answer);
 		System.out.println(board[3][0]);
+		
 		
 		for(int i=0; i<5; i++) {
 			for(int j=0; j<5; j++) {
