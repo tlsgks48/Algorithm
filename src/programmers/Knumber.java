9c4344cd642cd9a019da949c5397b0a80b62d9b3
tlsgks48 <tlsgks486@gmail.com>
2020-08-06 20:50:10 +0900

    프로그래머스 레벨1 k번째수 문제

---
+++ src/programmers/Knumber.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class Knumber {
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+		
+		// 문제: 배열 array의 i번째부터 j번째까지 자르고 정렬했을때 k번째 숫자를 구하는것.
+		
+		// 결론 : 결론은 짜르고 정렬해서 나온 k번째 위치의 값을 구하는것, 그리고 commands의 [i,j,k] 순으로 적용해서 나온 결과를 return에 담으시오.
+
+		int[] answer = {}, array;
+		int[][] commands;
+	}
+
+}
