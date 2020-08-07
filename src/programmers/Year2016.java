be88013510b387d6b92462285d252b33ed9b7393
tlsgks48 <tlsgks486@gmail.com>
2020-08-07 22:05:27 +0900

    프로그래머스 레벨1 2016년 문제

---
+++ src/programmers/Year2016.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class Year2016 {
+
+	public static void main(String[] args) {
+		// TODO 자동 생성된 메소드 스텁
+		
+		// 문제 : 2016년 a월 b일은 무슨요일 일까? 두 수 a,b를 입력받아 2016년 a월 b일이 무슨요일인지 리턴하는 문자열 함수 solution을 완성하라.
+		
+		// 1. 2016년은 윤년임. 2. 첫번째로 달력을 불러와야함.
+		
+		 String answer = "";
+		 int a, b;
+		 
+	}
+
+}
