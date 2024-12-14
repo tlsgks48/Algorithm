5f2aae5036ef38372eb426b16dabfc3dcc4a75b9
Han <tlsgks486@gmail.com>
2024-12-14 18:37:12 +0900

    [Add] 프로그래머스 level 0 - 덧셈식 출력하기

---
+++ src/programmers_level0/덧셈식출력하기.java
@@ -0,0 +1,15 @@
+package programmers_level0;
+
+import java.util.Scanner;
+
+public class 덧셈식출력하기 {
+    public class Solution {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println(a + " + " + b + " = " + (a + b));
+        }
+    }
+}
