40129d944ff9606c22bb30a87496b65da8312c58
Han <tlsgks486@gmail.com>
2024-12-15 18:57:47 +0900

    [Add] 프로그래머스 level 0 - 중앙값 구하기

---
+++ src/programmers_level0/중앙값구하기.java
@@ -0,0 +1,15 @@
+package programmers_level0;
+
+import java.util.Arrays;
+
+public class 중앙값구하기 {
+    public int solution(int[] array) {
+        int answer = 0;
+
+        Arrays.sort(array);
+
+        answer = array[array.length/2];
+
+        return answer;
+    }
+}
