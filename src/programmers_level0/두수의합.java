b4837274ab2110e06a2399811473c2405a10eeb0
Han <tlsgks486@gmail.com>
2024-12-17 21:51:52 +0900

    [Add] 프로그래머스 level 0 - 두 수의 합

---
+++ src/programmers_level0/두수의합.java
@@ -0,0 +1,11 @@
+package programmers_level0;
+
+public class 두수의합 {
+    public int solution(int num1, int num2) {
+        int answer = -1;
+
+        answer = num1 + num2;
+
+        return answer;
+    }
+}
