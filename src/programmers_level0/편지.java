fc34ca1ee9feb6b1e849acb784a201f0ca92b213
Han <tlsgks486@gmail.com>
2025-01-12 19:51:38 +0900

    [Add] 프로그래머스 level 0 - 편지

---
+++ src/programmers_level0/편지.java
@@ -0,0 +1,11 @@
+package programmers_level0;
+
+public class 편지 {
+    public int solution(String message) {
+        int answer = 0;
+
+        answer = message.length() * 2;
+
+        return answer;
+    }
+}
