d5622c92287a00d81a20fd7c8fe8a96ff44d86e6
Han <tlsgks486@gmail.com>
2025-01-16 21:52:21 +0900

    [Add] 프로그래머스 level 0 - 몫 구하기

---
+++ src/programmers_level0/몫구하기.java
@@ -0,0 +1,11 @@
+package programmers_level0;
+
+public class 몫구하기 {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+
+        answer = num1 / num2;
+
+        return answer;
+    }
+}
