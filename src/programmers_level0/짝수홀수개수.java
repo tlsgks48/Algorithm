f5b0c3c4108718d6c219eb3134a923702cf5f470
Han <tlsgks486@gmail.com>
2025-01-25 01:24:52 +0900

    [Add] 프로그래머스 level 0 - 짝수 홀수 개수

---
+++ src/programmers_level0/짝수홀수개수.java
@@ -0,0 +1,19 @@
+package programmers_level0;
+
+public class 짝수홀수개수 {
+    public int[] solution(int[] num_list) {
+        int[] answer = {};
+
+        answer = new int[2];
+
+        for(int i=0; i<num_list.length; i++) {
+            if(num_list[i] % 2 == 0) {
+                answer[0] += 1;
+            } else {
+                answer[1] += 1;
+            }
+        }
+
+        return answer;
+    }
+}
