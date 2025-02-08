284df4de166b2fa5c244a01eceb2cc7015ece683
Han <tlsgks486@gmail.com>
2025-02-08 09:36:00 +0900

    [Add] 프로그래머스 level 0 - 문자열 붙여서 출력하기

---
+++ src/programmers_level0/문자열붙여서출력하기.java
@@ -0,0 +1,13 @@
+package programmers_level0;
+
+import java.util.Scanner;
+
+public class 문자열붙여서출력하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        System.out.print(a+b);
+    }
+}
