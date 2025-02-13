3c73736201f47b91a7a4097f58b62268a6ab87a2
Han <tlsgks486@gmail.com>
2025-02-13 20:25:18 +0900

    [Add] 프로그래머스 level 0 - 배열 뒤집기

---
+++ src/programmers_level0/배열뒤집기.java
@@ -0,0 +1,15 @@
+package programmers_level0;
+
+public class 배열뒤집기 {
+    public int[] solution(int[] num_list) {
+        int[] answer = {};
+
+        answer = new int[num_list.length];
+
+        for(int i=0; i<num_list.length; i++) {
+            answer[i] = num_list[num_list.length - i - 1];
+        }
+
+        return answer;
+    }
+}
