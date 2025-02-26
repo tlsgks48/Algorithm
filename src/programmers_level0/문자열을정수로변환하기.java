3253611bd1a15be504c5f548d65e76bdfc44e11c
Han <tlsgks486@gmail.com>
2025-02-26 20:16:22 +0900

    [Add] 프로그래머스 level 0 - 문자열을 정수로 변환하기

---
+++ src/programmers_level0/문자열을정수로변환하기.java
@@ -0,0 +1,11 @@
+package programmers_level0;
+
+public class 문자열을정수로변환하기 {
+    public int solution(String n_str) {
+        int answer = 0;
+
+        answer = Integer.parseInt(n_str);
+
+        return answer;
+    }
+}
