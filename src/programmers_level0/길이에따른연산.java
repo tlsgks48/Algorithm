cd715748e522a871473c2af376f538d9c38d8092
Han <tlsgks486@gmail.com>
2025-03-13 10:01:03 +0900

    [Add] 프로그래머스 level 0 - 길이에 따른 연산

---
+++ src/programmers_level0/길이에따른연산.java
@@ -0,0 +1,20 @@
+package programmers_level0;
+
+public class 길이에따른연산 {
+    public int solution(int[] num_list) {
+        int answer = 0;
+
+        if(num_list.length > 10) {
+            for(int i=0; i<num_list.length; i++) {
+                answer += num_list[i];
+            }
+        } else {
+            answer = 1;
+            for(int i=0; i<num_list.length; i++) {
+                answer *= num_list[i];
+            }
+        }
+
+        return answer;
+    }
+}
