5fe2ccfe58df2d14acb015d9cc04df83387a31e6
Han <tlsgks486@gmail.com>
2025-03-25 23:37:49 +0900

    [Add] 프로그래머스 level 0 - 암호 해독

---
+++ src/programmers_level0/암호해독.java
@@ -0,0 +1,13 @@
+package programmers_level0;
+
+public class 암호해독 {
+    public String solution(String cipher, int code) {
+        String answer = "";
+
+        for(int i=0; i<cipher.length(); i++) {
+            if( (i+1) % code == 0) answer += "" + cipher.charAt(i);
+        }
+
+        return answer;
+    }
+}
