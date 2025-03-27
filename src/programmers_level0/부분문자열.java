62d0b067470ce51c4709241c9c8feaa5c2c89c6f
Han <tlsgks486@gmail.com>
2025-03-27 09:29:30 +0900

    [Add] 프로그래머스 level 0 - 부분 문자열

---
+++ src/programmers_level0/부분문자열.java
@@ -0,0 +1,11 @@
+package programmers_level0;
+
+public class 부분문자열 {
+    public int solution(String str1, String str2) {
+        int answer = 0;
+
+        if(str2.contains(str1)) answer = 1;
+
+        return answer;
+    }
+}
