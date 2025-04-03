a5b5230a5275a3ba111ff1aa41ba6f9bd7fb59e5
Han <tlsgks486@gmail.com>
2025-04-03 20:22:59 +0900

    [Add] 프로그래머스 level 0 - 가위 바위 보

---
+++ src/programmers_level0/가위바위보.java
@@ -0,0 +1,19 @@
+package programmers_level0;
+
+public class 가위바위보 {
+    public String solution(String rsp) {
+        String answer = "";
+
+        for(int i=0; i<rsp.length(); i++) {
+            if(rsp.charAt(i) == '2') {
+                answer += "0";
+            } else if(rsp.charAt(i) == '0') {
+                answer += "5";
+            } else if(rsp.charAt(i) == '5') {
+                answer += "2";
+            }
+        }
+
+        return answer;
+    }
+}
