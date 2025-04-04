bdb656d82eac3823df7c147a278fc1ab87928da8
Han <tlsgks486@gmail.com>
2025-04-04 21:41:43 +0900

    [Add] 프로그래머스 level 2 - 숫자의 표현

---
+++ src/programmers_level2/숫자의표현.java
@@ -0,0 +1,25 @@
+package programmers_level2;
+
+public class 숫자의표현 {
+    public int solution(int n) {
+        int answer = 0;
+        int sum = 0;
+
+        for(int i=1; i<=n; i++) {
+            sum = 0;
+            for(int j=i; j<=n; j++) {
+                sum += j;
+
+                if(sum > n) break;
+
+                if(sum == n) {
+                    answer++;
+                    break;
+                }
+            }
+
+        }
+
+        return answer;
+    }
+}
