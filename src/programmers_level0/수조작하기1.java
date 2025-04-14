e7f471a83f9eeda75373aae5c8d9b0bbcabbf276
Han <tlsgks486@gmail.com>
2025-04-14 22:37:33 +0900

    [Add] 프로그래머스 level 0 - 수 조작하기 1

---
+++ src/programmers_level0/수조작하기1.java
@@ -0,0 +1,21 @@
+package programmers_level0;
+
+public class 수조작하기1 {
+    public int solution(int n, String control) {
+        int answer = n;
+
+        for(int i=0; i<control.length(); i++) {
+            if(control.charAt(i) == 'w') {
+                answer += 1;
+            } else if(control.charAt(i) == 's') {
+                answer -= 1;
+            } else if(control.charAt(i) == 'd') {
+                answer += 10;
+            } else {
+                answer -= 10;
+            }
+        }
+
+        return answer;
+    }
+}
