82242feb980c69e4caaff46e271f73af8e0246d1
Han <tlsgks486@gmail.com>
2025-06-01 13:13:38 +0900

    [Add] 프로그래머스 level 0 - 글자 이어 붙여 문자열 만들기

---
+++ src/programmers_level0/글자이어붙여문자열만들기.java
@@ -0,0 +1,13 @@
+package programmers_level0;
+
+public class 글자이어붙여문자열만들기 {
+    public String solution(String my_string, int[] index_list) {
+        String answer = "";
+
+        for(int i=0; i<index_list.length; i++) {
+            answer += "" + my_string.charAt(index_list[i]);
+        }
+
+        return answer;
+    }
+}
