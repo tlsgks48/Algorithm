416574c7782e43fd7939c472df2118aa6c9b39da
Han <tlsgks486@gmail.com>
2025-06-15 13:33:20 +0900

    [Add] 프로그래머스 level 0 - 개미 군단

---
+++ src/programmers_level0/개미군단.java
@@ -0,0 +1,22 @@
+package programmers_level0;
+
+public class 개미군단 {
+    public int solution(int hp) {
+        int answer = 0;
+        int sum = hp % 5;
+
+        answer += hp/5;
+
+        if(sum % 3 == 0) {
+
+            answer += sum /= 3;
+
+        } else {
+
+            answer += (sum /= 3) + (sum % 3);
+
+        }
+
+        return answer;
+    }
+}
